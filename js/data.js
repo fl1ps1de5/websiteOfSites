43e2c3972ef1852c67f3fcf46c9342aba7e5ffe5
Ethan Hirschowitz <ehir9923@uni.sydney.edu.au>
2024-02-29 01:10:19 +1100

    Added /css, /pages, some new pages, and /js which has a database for websites and a script to display them

---
+++ js/data.js
@@ -0,0 +1,16 @@
+const websites = [ 
+    { 
+        name: "Adobe Package Manager", 
+        url: "https://github.com/Drovosek01/adobe-packager", 
+        category: "utilities", 
+        brief: `A github repo which contains a script that can basically download any version of any Adobe app (for free!).
+        Really cool command-line app, and really useful. It does require AdobeZii \n to patch the file though, and also
+        you need to delete adobe creative cloud off your device and download a version 5.7.0.1307. Adobe Zii can be found online.`
+    },
+    { 
+        name: "Looptap", 
+        url: "https://looptap.vasanthv.com", 
+        category: "games", 
+        brief: "An easy, fun, timing-based arcade style game. Fun for 10 minutes. My best is 232."
+    }
+  ];
---
+++ js/script.js
@@ -0,0 +1,22 @@
+function displayWebsites(category) {
+    const container = document.getElementById('sites'); // Ensure you have a container with this ID in your HTML
+    container.innerHTML = ''; // Clear existing content
+  
+    const filteredWebsites = websites.filter(website => category === 'all' ? true : website.category === category);
+  
+    filteredWebsites.forEach(website => {
+        const item = document.createElement('div');
+        item.classList.add('website-item');
+    
+        item.innerHTML = `
+            <h2 class="website-name"><a href="${website.url}" target="_blank" class="website-link">${website.name}</a></h2>
+            <p class="website-brief">${website.brief}</p>
+        `;
+        container.appendChild(item);
+    });
+    
+  }
+
+
+  
+  
