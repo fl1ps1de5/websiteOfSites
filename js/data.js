d17243fdfe739dc25ee467658b090f051649c4e8
Ethan Hirschowitz <ehir9923@uni.sydney.edu.au>
2024-04-03 15:10:01 +1100

    Added a few dles

--- js/data.js
+++ js/data.js
@@ -3,38 +3,76 @@ const websites = [
         name: "Excalidraw", 
         url: "https://excalidraw.com/", 
         category: "utilities", 
-        brief: `Super easy to use, lightweight NO SIGN IN, drawing thingo - also good for flow charts + 
+        brief: `Easy to use, lightweight NO SIGN UP, drawing thingo - also good for flow charts + 
         it can be used for collaborative stuff like group projects.`
     },
     { 
         name: "Angle", 
         url: "https://angle.wtf/", 
         category: "games", 
-        brief: "Wordle like game where you guess the angle."
+        brief: "Wordle like game where you guess an angle."
     },
     { 
         name: "Neal.fun", 
         url: "https://neal.fun/", 
         category: "games", 
-        brief: "Pretty nice collection of fun little games / time wasters. Some of them are rather interesting. All pretty well made."
+        brief: "Collection of fun little games & time wasters. Some of them are rather interesting. All pretty well made."
     },
     { 
         name: "Simon Tatham's Portable Puzzle Collection", 
         url: "https://www.chiark.greenend.org.uk/~sgtatham/puzzles/", 
         category: "games", 
         brief: `A bunch of fun, interesting puzzles, which run in the browser. 
-        Some pretty out of the box games too. All hosted on an old school, low-effort website, which adds 
-        to the charm.   I say "low-effort", but the actual coding for some of the games seems like it would be
-        pretty nutty (look at the shape options in Cube)`
+        Some pretty out of the box games too. Also comes with documentation for the programming of the games for nerds (me)`
     },
     { 
         name: "Evil AI Quiz", 
         url: "https://quiz.cord.com/", 
         category: ["games", "interesting"], 
-        brief: `This is a really cool little website. Its basically a quiz where you and an AI LLM have to work together to 
-        submit the answer to various questions. Some are hard for humans and easy for the AI, and some are the other way around
-        Does a good job at showing the weaknesses of AI, and its pretty fun.`
+        brief: `Basically a quiz where you and an AI LLM have to work together to 
+        submit the answer to various questions. Some are hard for humans and easy for the AI, and some are the other way around.`
+    },
+    { 
+        name: "Strands", 
+        url: "https://www.nytimes.com/games/strands", 
+        category: ["games", "daily"], 
+        brief: "Daily NYT game, where you have to find words from a cryptic theme."
+    },
+    { 
+        name: "Connections", 
+        url: "https://www.nytimes.com/games/connections", 
+        category: ["games", "daily"], 
+        brief: "Daily NYT game, where you have to group words based on their theme."
+    },
+    { 
+        name: "Mini Crossword", 
+        url: "https://www.nytimes.com/crosswords/game/mini", 
+        category: ["games", "daily"], 
+        brief: "Daily NYT mini-crossword. Should take like 40s to do if its a good attempt"
+    },
+    { 
+        name: "Gamdle", 
+        url: "https://www.gamedle.wtf/?lang=en", 
+        category: ["games", "daily"], 
+        brief: "A few daily video-game based 'dle' games - pretty fun and right up my alley. 'Guess' is easily the most fun mode."
+    },
+    { 
+        name: "Timeguessr", 
+        url: "https://www.timeguessr.com/", 
+        category: ["games", "daily"], 
+        brief: "Daily game where you guess a location and year that a photo was taken, for 5 rounds."
+    },
+    { 
+        name: "Daily Tomato", 
+        url: "https://www.rottentomatoes.com/movie-trivia/", 
+        category: ["games", "daily"], 
+        brief: "Daily rotten-tomatoes provided movie guessing game. Not tooooooo hard."
     }
+
+
+    
+
+
     // { 
     //     name: "", 
     //     url: "", 
